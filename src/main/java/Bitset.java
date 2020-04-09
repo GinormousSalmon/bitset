d9180edbbf25383e88996b92241b323e24e589b8
GinormousSalmon <terleczcky.alex@gmail.com>
2020-04-09 23:56:42 +0300

    written correct version of Bitset written all tests

--- src/main/java/Bitset.java
+++ src/main/java/Bitset.java
@@ -1,92 +1,180 @@
 package main.java;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.lang.reflect.Array;
 import java.util.*;
 
 
-public class Bitset<T> implements Iterable<T> {
-    private ArrayList<T> data = new ArrayList<>();
-
-    public Bitset() {
+class prog {
+    public static void print(Object line) {
+        System.out.print(line);
     }
 
-    public Bitset(T input) {
-        this.add(input);
+    public static void println(Object line) {
+        System.out.println(line);
     }
 
-    public Bitset(T[] input) {
-        this.add(input);
+    public static void main(String[] args) {
+        Bitset bitsetA = new Bitset(20);
+        Bitset bitsetB = new Bitset(30);
+        int[] a = {1, 2, 3, 4, 5, 10, 14, 15, 16};
+        int[] b = {0, 2, 3, 11, 12, 14, 15, 16, 17, 19, 29};
+        bitsetA.add(a);
+        bitsetB.add(b);
+        bitsetA.print();
+        bitsetB.print();
+        Bitset bitsetC = bitsetB.intersect(bitsetA);
+        bitsetC.print();
+    }
+}
+
+
+public class Bitset {//<T> implements Iterable<T> {
+    private int length;
+    private byte[] data;
+
+
+    public Bitset(int N) {
+        if (N > 0) {
+            this.length = N;
+            this.data = new byte[(int) Math.ceil((double) N / 8)];
+            Arrays.fill(this.data, (byte) -128);
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public Bitset(Collection<T> input) {
-        this.add(input);
+    public int length() {
+        return this.length;
     }
 
-    public boolean contains(T elem) {
-        return this.data.contains(elem);
+    //булевы флаги "упаковываются" в тип byte и хранятся в byte[]
+    public boolean[] toBinary(byte number) {
+        int n = number + 128;
+        boolean[] bin = new boolean[8];
+        for (int i = 0; i < 8; i++) {
+            bin[7 - i] = n % 2 == 1;
+            n /= 2;
+        }
+        return bin;
     }
 
-    public void add(T elem) {
-        if (!this.data.contains(elem))
-            this.data.add(elem);
+    public byte toDecimal(boolean[] bin) {
+        byte dec = -128;
+        int[] coefs = {1, 2, 4, 8, 16, 32, 64, 128};
+        for (int i = 0; i < 8; i++)
+            if (bin[7 - i])
+                dec += coefs[i];
+        return dec;
     }
 
-    public void add(T[] elem) {
-        for (T i : elem)
-            if (!this.data.contains(i))
-                this.data.add(i);
+    public void add(int n) {
+        if (n >= 0 && n < this.length) {
+            int byteIndex = n / 8;
+            boolean[] bin = toBinary(this.data[byteIndex]);
+            bin[n % 8] = true;
+            this.data[byteIndex] = toDecimal(bin);
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public void add(Collection<T> elem) {
-        for (T i : elem)
-            if (!this.data.contains(i))
-                this.data.add(i);
+    public void add(int[] numbers) {
+        for (int n : numbers)
+            if (n >= 0 && n < this.length) {
+                int byteIndex = n / 8;
+                boolean[] bin = toBinary(this.data[byteIndex]);
+                bin[n % 8] = true;
+                this.data[byteIndex] = toDecimal(bin);
+            } else {
+                throw new IllegalArgumentException();
+            }
     }
 
-    public void remove(T elem) {
-        this.data.remove(elem);
+    public void remove(int n) {
+        if (n >= 0 && n < this.length) {
+            int byteIndex = n / 8;
+            boolean[] bin = toBinary(this.data[byteIndex]);
+            bin[n % 8] = false;
+            this.data[byteIndex] = toDecimal(bin);
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public void remove(T[] elem) {
-        for (T i : elem)
-            this.data.remove(i);
+    public void remove(int[] numbers) {
+        for (int n : numbers)
+            if (n >= 0 && n < this.length) {
+                int byteIndex = n / 8;
+                boolean[] bin = toBinary(this.data[byteIndex]);
+                bin[n % 8] = false;
+                this.data[byteIndex] = toDecimal(bin);
+            } else {
+                throw new IllegalArgumentException();
+            }
     }
 
-    public void remove(Collection<T> elem) {
-        this.data.removeAll(elem);
+    public boolean contains(int n) {
+        if (n >= 0 && n < this.length)
+            return toBinary(this.data[n / 8])[n % 8];
+        else
+            throw new IllegalArgumentException();
     }
 
-    public void intersection(Bitset<T> a) {
-        for (T i : new Bitset<T>(this.toList()))
-            if (!a.contains(i))
-                this.remove(i);
+    public void print() {
+        for (byte b : this.data) {
+            for (boolean bit : toBinary(b))
+                System.out.print(bit ? "1" : "0");
+            System.out.print(".");
+        }
+        System.out.println();
     }
 
-    public void union(Bitset<T> a) {
-        for (T i : a)
-            if (!this.contains(i))
-                this.add(i);
+    public void difference() {
+        for (int i = 0; i < this.data.length; i++) {
+            this.data[i] = (byte) (-this.data[i] - (byte) 1);
+        }
     }
 
-    public ArrayList<T> toList() {
-        return this.data;
+
+    public Bitset union(Bitset bitset) {
+        Bitset result;
+        if (this.length > bitset.length) {
+            result = this;
+            for (int i = 0; i < bitset.data.length; i++)
+                result.data[i] = (byte) (((result.data[i] + 128) | (bitset.data[i] + 128)) - 128);
+        } else {
+            result = bitset;
+            for (int i = 0; i < this.data.length; i++)
+                result.data[i] = (byte) (((result.data[i] + 128) | (this.data[i] + 128)) - 128);
+        }
+        return result;
     }
 
-    @NotNull
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
+    public Bitset intersect(Bitset bitset) {
+        Bitset result;
+        if (bitset.length < this.length) {
+            result = bitset;
+            for (int i = 0; i < bitset.data.length; i++)
+                result.data[i] = (byte) (((result.data[i] + 128) & (this.data[i] + 128)) - 128);
+        } else {
+            result = this;
+            for (int i = 0; i < this.data.length; i++)
+                result.data[i] = (byte) (((result.data[i] + 128) & (bitset.data[i] + 128)) - 128);
+        }
+        return result;
+    }
+
+    public Iterator<Boolean> iterator() {
+        return new Iterator<>() {
             private int currentIndex = 0;
 
             @Override
             public boolean hasNext() {
-                return currentIndex < data.size();
+                return currentIndex < data.length;
             }
 
             @Override
-            public T next() {
-                return data.get(currentIndex++);
+            public Boolean next() {
+                return contains(currentIndex++);
             }
 
             @Override
--- src/test/BitSetTest.java
+++ src/test/BitSetTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import jdk.jfr.Unsigned;
 import main.java.Bitset;
 
 import java.security.cert.TrustAnchor;
@@ -11,94 +12,146 @@ import org.junit.jupiter.api.*;
 public class BitSetTest {
     Random rand = new Random();
 
-    public static void print(Object line) {
+    public static void println(Object line) {
         System.out.println(line);
     }
 
-    @Test
-    void addTest() {
-        Bitset<Float> data = new Bitset<>();
-        for (int i = 0; i < 1000; i++) {
-            int in = (int) (rand.nextFloat() * 100);
-            float fl = ((float) in) / 100;
-            data.add(fl);
-        }
-        ArrayList<Float> a = data.toList();
-        Collections.sort(a);
-        for (int i = 0; i < a.size() - 1; i++)
-            Assertions.assertNotEquals(a.get(i), a.get(i + 1));
+    public static void print(Object line) {
+        System.out.print(line);
     }
 
     @Test
-    void removeTest() {
-        Bitset<Integer> data = new Bitset<>();
-        ArrayList<Integer> numbers = new ArrayList<>();
+    void toBinaryTest() {
+        int intToByteDivider = (int) Math.pow(2, 24);
+        Bitset bitset = new Bitset(8);
         for (int i = 0; i < 1000; i++) {
-            int rand_int = rand.nextInt();
-            data.add(rand_int);
-            numbers.add(rand_int);
-        }
-        for (int i : numbers){
-            data.remove(i);
-            Assertions.assertFalse(data.contains(i));
+            byte randomByte = (byte) (rand.nextInt() / intToByteDivider);
+            boolean[] bits = new boolean[8];
+            char[] chars = Integer.toBinaryString(randomByte + 128).toCharArray();
+            for (int j = 0; j < chars.length; j++)
+                bits[j + 8 - chars.length] = chars[j] == '1';
+            Assertions.assertArrayEquals(bitset.toBinary(randomByte), bits);
         }
     }
 
     @Test
-    void intersectionTest(){
-        Bitset<Float> data1 = new Bitset<>();
+    void toDecimalTest() {
+        Bitset bitset = new Bitset(8);
         for (int i = 0; i < 1000; i++) {
-            int in = (int) (rand.nextFloat() * 2000);
-            float fl = ((float) in) / 2000;
-            data1.add(fl);
+            boolean[] bits = new boolean[8];
+            String binString = "";
+            for (int j = 0; j < 8; j++) {
+                bits[j] = rand.nextBoolean();
+                binString = binString.concat(bits[j] ? "1" : "0");
+            }
+            int number = Integer.parseInt(binString, 2) - 128;
+            Assertions.assertEquals(bitset.toDecimal(bits), number);
         }
-
-        Bitset<Float> data2 = new Bitset<>();
-        for (int i = 0; i < 1000; i++) {
-            int in = (int) (rand.nextFloat() * 2000);
-            float fl = ((float) in) / 2000;
-            data2.add(fl);
-        }
-
-        ArrayList<Float> a1 = new ArrayList<>(data1.toList());
-        a1.removeAll(data2.toList());
-
-        ArrayList<Float> a2 = new ArrayList<>(data1.toList());
-        a2.removeAll(a1);
-        Collections.sort(a2);
-
-        data1.intersection(data2);
-        a1 = new ArrayList<>(data1.toList());
-        Collections.sort(a1);
-        Assertions.assertIterableEquals(a1, a2);
     }
 
     @Test
-    void unionTest(){
-        Bitset<Float> data1 = new Bitset<>();
-        for (int i = 0; i < 1000; i++) {
-            int in = (int) (rand.nextFloat() * 2000);
-            float fl = ((float) in) / 2000;
-            data1.add(fl);
+    void addRemoveTest() {
+        for (int i = 0; i < 50; i++) {
+            int size = (int) (rand.nextFloat() * 50000);
+            Bitset bitset = new Bitset(size);
+            for (int j = 0; j < 10000; j++) {
+                int random = (int) (rand.nextFloat() * 1000000);
+                if (random < size) {
+                    bitset.add(random);
+                    Assertions.assertTrue(bitset.contains(random));
+                    bitset.remove(random);
+                    Assertions.assertFalse(bitset.contains(random));
+                } else {
+                    Assertions.assertThrows(IllegalArgumentException.class, () -> bitset.add(random));
+                    Assertions.assertThrows(IllegalArgumentException.class, () -> bitset.remove(random));
+                }
+            }
         }
-
-        Bitset<Float> data2 = new Bitset<>();
-        for (int i = 0; i < 1000; i++) {
-            int in = (int) (rand.nextFloat() * 2000);
-            float fl = ((float) in) / 2000;
-            data2.add(fl);
-        }
-
-        HashSet<Float> hash1 = new HashSet<>(data1.toList());
-        HashSet<Float> hash2 = new HashSet<>(data2.toList());
-        hash1.addAll(hash2);
-        ArrayList<Float> a1 = new ArrayList<>(hash1);
-        Collections.sort(a1);
-
-        data1.union(data2);
-        ArrayList<Float> a2 = new ArrayList<>(data1.toList());
-        Collections.sort(a2);
-        Assertions.assertIterableEquals(a1, a2);
     }
 
+    @Test
+    void differenceTest() {
+        for (int a = 0; a < 100; a++) {
+            int size = (int) (rand.nextFloat() * 50000);
+            Bitset bitset = new Bitset(size);
+            ArrayList<Integer> trueBits = new ArrayList<>();
+            for (int j = 0; j < 10000; j++) {
+                int random = (int) (rand.nextFloat() * 1000000);
+                if (random < size) {
+                    trueBits.add(random);
+                    bitset.add(random);
+                }
+            }
+            bitset.difference();
+            for (int i = 0; i < size; i++)
+                if (trueBits.contains(i))
+                    Assertions.assertFalse(bitset.contains(i));
+                else
+                    Assertions.assertTrue(bitset.contains(i));
+        }
+    }
+
+    @Test
+    void unionTest() {
+        for (int a = 0; a < 100; a++) {
+            int size1 = (int) (rand.nextFloat() * 50000);
+            int size2 = (int) (rand.nextFloat() * 50000);
+            Bitset bitset1 = new Bitset(size1);
+            Bitset bitset2 = new Bitset(size2);
+            ArrayList<Integer> trueBits1 = new ArrayList<>();
+            ArrayList<Integer> trueBits2 = new ArrayList<>();
+            for (int j = 0; j < 10000; j++) {
+                int random = (int) (rand.nextFloat() * 1000000);
+                if (random < size1) {
+                    trueBits1.add(random);
+                    bitset1.add(random);
+                }
+            }
+            for (int j = 0; j < 10000; j++) {
+                int random = (int) (rand.nextFloat() * 1000000);
+                if (random < size2) {
+                    trueBits2.add(random);
+                    bitset2.add(random);
+                }
+            }
+            Bitset result = bitset1.union(bitset2);
+            for (int i = 0; i < result.length(); i++)
+                if (trueBits1.contains(i) || trueBits2.contains(i))
+                    Assertions.assertTrue(result.contains(i));
+                else
+                    Assertions.assertFalse(result.contains(i));
+        }
+    }
+
+    @Test
+    void intersectTest() {
+        for (int a = 0; a < 100; a++) {
+            int size1 = (int) (rand.nextFloat() * 50000);
+            int size2 = (int) (rand.nextFloat() * 50000);
+            Bitset bitset1 = new Bitset(size1);
+            Bitset bitset2 = new Bitset(size2);
+            ArrayList<Integer> trueBits1 = new ArrayList<>();
+            ArrayList<Integer> trueBits2 = new ArrayList<>();
+            for (int j = 0; j < 10000; j++) {
+                int random = (int) (rand.nextFloat() * 1000000);
+                if (random < size1) {
+                    trueBits1.add(random);
+                    bitset1.add(random);
+                }
+            }
+            for (int j = 0; j < 10000; j++) {
+                int random = (int) (rand.nextFloat() * 1000000);
+                if (random < size2) {
+                    trueBits2.add(random);
+                    bitset2.add(random);
+                }
+            }
+            Bitset result = bitset1.intersect(bitset2);
+            for (int i = 0; i < result.length(); i++)
+                if (trueBits1.contains(i) && trueBits2.contains(i))
+                    Assertions.assertTrue(result.contains(i));
+                else
+                    Assertions.assertFalse(result.contains(i));
+        }
+    }
 }
